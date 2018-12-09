df3bf6e8615088ee589a4fb7c4d43ae9e5fe80fd
Rahul <j.rahulcse@gmail.com>
2018-12-09 13:58:12 +0530

    Updated script in Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,6 +2,8 @@ package stepDefinitions;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -19,7 +21,7 @@ public class Hooks
 {
 	TestContext testContext;
 	WebDriver driver;
-	
+
 	public Hooks(TestContext context)
 	{
 		testContext = context;
@@ -30,34 +32,38 @@ public class Hooks
 	{
 		driver = testContext.getWebDriverManager().getDriver();
 	}
-	
-	
+
+
 	@After(order = 1)
-	 public void afterScenario(Scenario scenario) {
-	 if (scenario.isFailed()) {
-	 String screenshotName = scenario.getName().replaceAll(" ", "_");
-	 try {
-	 //This takes a screenshot from the driver at save it to the specified location
-	 File sourcePath = ((TakesScreenshot) testContext.getWebDriverManager().getDriver()).getScreenshotAs(OutputType.FILE);
-	 
-	 //Building up the destination path for the screenshot to save
-	 //Also make sure to create a folder 'screenshots' with in the cucumber-report folder
-	 File destinationPath = new File(System.getProperty("user.dir") + "/target/failure_screenshots/" + screenshotName + ".png");
-	 
-	 //Copy taken screenshot from source location to destination location
-	 Files.copy(sourcePath, destinationPath);   
-	 
-	 //This attach the specified screenshot to the test
-	 Reporter.addScreenCaptureFromPath(destinationPath.toString());
-	 } catch (IOException e) {
-	 } 
-	 }
-	 }
-	 
-	 
-	 @After(order = 0)
-	 public void AfterSteps() throws Exception {
-		 Thread.sleep(3000);
-	 testContext.getWebDriverManager().closeDriver();
-	 }
+	public void afterScenario(Scenario scenario) {
+		if (scenario.isFailed()) {
+			String screenshotName = scenario.getName().replaceAll(" ", "_");
+			try {
+				//This takes a screenshot from the driver at save it to the specified location
+				File sourcePath = ((TakesScreenshot) testContext.getWebDriverManager().getDriver()).getScreenshotAs(OutputType.FILE);
+
+				//Building up the destination path for the screenshot to save
+				//Also make sure to create a folder 'screenshots' with in the cucumber-report folder
+				File destinationPath = new File(System.getProperty("user.dir") + "/target/cucumber-reports/failure_screenshots/" + screenshotName + "_" + timestamp() + ".png");
+
+				//Copy taken screenshot from source location to destination location
+				Files.copy(sourcePath, destinationPath);   
+
+				//This attach the specified screenshot to the test
+				Reporter.addScreenCaptureFromPath(destinationPath.toString());
+			} catch (IOException e) {
+			} 
+		}
+	}
+
+	public String timestamp() {
+		return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
+	}
+
+
+	@After(order = 0)
+	public void AfterSteps() throws Exception {
+		Thread.sleep(3000);
+		testContext.getWebDriverManager().closeDriver();
+	}
 }
